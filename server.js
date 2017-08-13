649cd1a51e4660f582febdfed117ac1eea07d89e
dliszka <dliszka@crodu.com>
2017-08-13 20:22:56 +0200

    change for chosing port

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-var dotenvLoad = require('dotenv').load;
-const env = dotenvLoad({ path: '.env' }).parsed || process.env
-
 var app = require("express")();
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
 
-var port = env.PORT || 3000;
+var port = process.env.PORT || 4000;
 
 let socketArray = [];
 
