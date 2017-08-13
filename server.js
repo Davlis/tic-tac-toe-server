5c7b74714e5ee826ebaadd5b4271079302082544
dliszka <dliszka@crodu.com>
2017-08-13 16:40:42 +0200

    tour handling

--- server.js
+++ server.js
@@ -7,9 +7,31 @@ var io = require("socket.io")(http);
 
 var port = env.PORT || 3000;
 
+let socketArray = [];
+
 io.on('connection', (socket) => {
+  socket.on('new_player', (playerId) => {
+    socketArray.push(socket);
+    if(socketArray.length === 2) {
+      socketArray[0].emit('player_move');
+    } else {
+      socket.emit('wait');
+    }
+  });
+
   socket.on('move', (state) => {
     io.emit('state', state);
+    let _socket = socketArray.filter(s => s.id !== socket.id)[0];
+    if(_socket) {
+      _socket.emit('player_move');
+    } else {  // if someone breaks js on frontside
+      socket.emit('wait');
+    }
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('reset');
+    socketArray = socketArray.filter(s => s.id !== socket.id);
   });
 });
 
